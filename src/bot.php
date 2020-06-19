7f194999e1e28d427b03707c8d99d845eb26fb54
Vladius25 <vkorol2509@icloud.com>
2020-06-20 01:51:59 +0500

    Connect bot with auth

--- src/bot.php
+++ src/bot.php
@@ -4,12 +4,14 @@ use VK\CallbackApi\Server\VKCallbackApiServerHandler;
 use VK\Client\VKApiClient;
 
 require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/utils.php';
 
 class ServerHandler extends VKCallbackApiServerHandler
 {
     const SECRET = 'Oquuo5aiChei6ah';
     const GROUP_ID = 152396040;
-    const CONFIRMATION_TOKEN = '23981ea6';
+    const CONFIRMATION_TOKEN = 'd2a60eda';
     const ACCESS_TOKEN = '5f9071c17793443349971f3dc00ac71fa16d25e1c81cd39e8cb6447075499627dc4c9108a0713785dc9bb';
 
     function confirmation(int $group_id, ?string $secret)
@@ -21,12 +23,23 @@ class ServerHandler extends VKCallbackApiServerHandler
 
     public function messageNew(int $group_id, ?string $secret, array $object)
     {
-        $vk = new VKApiClient('5.110');
         $from = $object['message']->from_id;
-        $text_data = $object['message']->text;
-        $splitted_dates = explode('-', $text_data);
-        $date_start = strtotime($splitted_dates[0]);
-        $date_end = strtotime($splitted_dates[1]);
+        if($from == 135641618) {
+            $vk = new VKApiClient('5.110');
+            $auth = new Authorization();
+            $user_token = $auth->getToken();
+            if(is_null($user_token)) {
+                Utils::sendMsg($vk, static::ACCESS_TOKEN, $from, "Необходимо автопизоваться");
+                Utils::sendMsg($vk, static::ACCESS_TOKEN, $from, $auth->makeTokenRequest());
+                exit(0);
+            }
+            $message_text = $object['message']->text;
+            $splitted_dates = explode('-', $message_text);
+            $timestamp_from = strtotime($splitted_dates[0]);
+            $timestamp_to = strtotime($splitted_dates[1]);
+            $result = Utils::getLids($vk, $user_token, static::GROUP_ID, $timestamp_from, $timestamp_to);
+            Utils::sendMsg($vk, static::ACCESS_TOKEN, $from, 'test');
+        }
         echo 'ok';
     }
 }
