fc10e8f9693e2a5f54f546c5a7133aae2675a4e1
Vladius25 <vkorol2509@icloud.com>
2020-06-20 16:25:04 +0500

    Fix require in auth

--- src/auth.php
+++ src/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/utils.php';
+require_once __DIR__ . '/utils.php';
 
 use VK\Client\VKApiClient;
 use VK\Exceptions\VKApiException;
