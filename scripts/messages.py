de9c45c6e8ac88a2426b3c24b7340fe1221147b1
Vladius25 <vkorol2509@icloud.com>
2020-06-21 21:35:51 +0500

    Refactor messages script

--- scripts/messages.py
+++ scripts/messages.py
@@ -2,36 +2,57 @@
 Скрипт предназачен для заполнения PostgreSQL
 данными о первом сообщении в каждой переписке сообщества в вк
 """
-import itertools
+import sys
+from itertools import count
 
 import psycopg2
 from tqdm import tqdm
 from vk_api import VkApi
 
-community_token = "5f9071c17793443349971f3dc00ac71fa16d25e1c81cd39e8cb6447075499627dc4c9108a0713785dc9bb"
-vk = VkApi(token=community_token).get_api()
 
-users = []
-for offset in itertools.count(start=1, step=200):
-    messages = vk.messages.getConversations(offset=offset, count=200)
-    if not messages["items"]:
-        break
-    for item in messages["items"]:
-        if item["conversation"]["peer"]["type"] == "user":
-            users.append(item["conversation"]["peer"]["id"])
+def collect_conversations(vk):
+    conversations = []
+    for offset in count(start=0, step=200):
+        messages = vk.messages.getConversations(offset=offset, count=200)
+        if not messages["items"]:
+            break
+        for item in messages["items"]:
+            if item["conversation"]["peer"]["type"] == "user":
+                conversations.append(item["conversation"]["peer"]["id"])
+    return conversations
 
 
-conn = psycopg2.connect(user="postgres", password="developing", host="127.0.0.1", port=5432, database="vkstatbot")
-cursor = conn.cursor()
-try:
-    cursor.execute("CREATE TABLE first_msg (id serial PRIMARY KEY, user_id integer, date timestamp);")
-except psycopg2.errors.DuplicateTable:
-    cursor.execute('ROLLBACK;')
-    cursor.execute('TRUNCATE first_msg;')
+def create_bd_table(cursor):
+    cursor.execute('DROP TABLE IF EXISTS first_msg;')
+    cursor.execute("CREATE TABLE first_msg (id serial PRIMARY KEY, group_id int NOT NULL, user_id int NOT NULL, "
+                   "date timestamp, unique(group_id, user_id));")
 
-for user_id in tqdm(users):
-    date = vk.messages.getHistory(offset=0, count=1, user_id=user_id)["items"][0]["date"]
-    cursor.execute("INSERT INTO first_msg (user_id, date) VALUES (%s, to_timestamp(%s))", (user_id, date))
 
-conn.commit()
-conn.close()
+def insert_data_in_bd(vk, cursor, converstaions, group_id):
+    for user_id in tqdm(converstaions):
+        date = vk.messages.getHistory(offset=0, count=1, user_id=user_id)["items"][0]["date"]
+        cursor.execute("INSERT INTO first_msg (group_id, user_id, date) VALUES (%s, %s, to_timestamp(%s))",
+                       (group_id, user_id, date))
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Нужно указать id группы и ее токен:")
+        print("python messages.py <group_id> <access_token>")
+        exit(1)
+    group_id, community_token = sys.argv[1], sys.argv[2]
+    vk = VkApi(token=community_token).get_api()
+
+    conn = psycopg2.connect(user="postgres", password="developing", host="127.0.0.1", port=5432, database="vkstatbot")
+    cursor = conn.cursor()
+    create_bd_table(cursor)
+
+    conversations = collect_conversations(vk)
+    insert_data_in_bd(vk, cursor, conversations, group_id)
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
