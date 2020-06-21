ff0a474c0f51ffd1c084d7a4daea52f236886d28
Vladius25 <vkorol2509@icloud.com>
2020-06-21 17:40:33 +0500

    Python script to init db

--- scripts/messages.py
+++ scripts/messages.py
@@ -1,3 +1,10 @@
+"""
+Скрипт предназачен для заполнения PostgreSQL
+данными о первом сообщении в каждой переписке сообщества в вк
+"""
+import itertools
+
+import psycopg2
 from tqdm import tqdm
 from vk_api import VkApi
 
@@ -5,7 +12,7 @@ community_token = "5f9071c17793443349971f3dc00ac71fa16d25e1c81cd39e8cb6447075499
 vk = VkApi(token=community_token).get_api()
 
 users = []
-for offset in range(0, 10000000, 200):
+for offset in itertools.count(start=1, step=200):
     messages = vk.messages.getConversations(offset=offset, count=200)
     if not messages["items"]:
         break
@@ -13,7 +20,18 @@ for offset in range(0, 10000000, 200):
         if item["conversation"]["peer"]["type"] == "user":
             users.append(item["conversation"]["peer"]["id"])
 
-dates = []
+
+conn = psycopg2.connect(user="postgres", password="developing", host="127.0.0.1", port=5432, database="vkstatbot")
+cursor = conn.cursor()
+try:
+    cursor.execute("CREATE TABLE first_msg (id serial PRIMARY KEY, user_id integer, date timestamp);")
+except psycopg2.errors.DuplicateTable:
+    cursor.execute('ROLLBACK;')
+    cursor.execute('TRUNCATE first_msg;')
+
 for user_id in tqdm(users):
-    dates.append(vk.messages.getHistory(offset=0, count=1, user_id=user_id)["items"][0]["date"])
-print(dates)
+    date = vk.messages.getHistory(offset=0, count=1, user_id=user_id)["items"][0]["date"]
+    cursor.execute("INSERT INTO first_msg (user_id, date) VALUES (%s, to_timestamp(%s))", (user_id, date))
+
+conn.commit()
+conn.close()
